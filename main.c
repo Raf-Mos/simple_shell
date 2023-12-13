ac1ed4b63c6fc6703419f39f5ebabb4382f13c3a
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-12-13 13:31:58 +0100

    add man function

---
+++ main.c
@@ -0,0 +1,36 @@
+#include"shell.h"
+
+/**
+* main - simple shell main function
+* @ac: input
+* @av: input arg
+* Return: status
+*/
+int main(int ac, char **av)
+{
+	char *buf = NULL, **cmd = NULL;
+	int status = 0, counter = 0;
+	(void)ac;
+
+	while (1)
+	{
+		buf = reader();
+		if (buf == NULL)
+		{
+			if (isatty(STDIN_FILENO) == 1)
+				write(STDOUT_FILENO, "\n", 1);
+		return (status);
+		}
+		counter++;
+		cmd = splitter(buf);
+		if (cmd == NULL)
+			continue;
+
+		if (_strcmp(cmd[0], "exit") == 0)
+			exit_sh(char **cmd, char **av, int *status, int counter);
+		else if (_strcmp(cmd[0], "env") == 0)
+			full_env(cmd, &status);
+		else
+			status = executer(cmd, av, counter);
+	}
+}
