43d24e9db32b19d5487684a203f9675f198c7381
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-12-13 13:50:10 +0100

    add get_func02.c

---
+++ get_func02.c
@@ -0,0 +1,47 @@
+#include"shell.h"
+
+/**
+ * positive_num - check if s is a positive number
+ * @s: the number to check
+ *
+ * Return: 1 on success, 0 on failure
+*/
+int positive_num(char *s)
+{
+	int i = 0;
+
+	if (!s)
+	{
+		return (0);
+	}
+	while (s != NULL)
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+*/
+
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	do {
+	if (*s == '-')
+		sign *= -1;
+	else if (*s >= '0' && *s <= '9')
+		num = (num * 10) + (*s - '0');
+	else if (num > 0)
+		break;
+	} while (*s++);
+
+	return (num * sign);
+}
